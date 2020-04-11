48f522d375f6c42d85fe2c26d20c17806cc3af2a
Stanislaw <52160413+0Stanislaw0@users.noreply.github.com>
2020-04-11 14:51:45 +0300

    Update pipelines.py

--- parserWB/pipelines.py
+++ parserWB/pipelines.py
@@ -7,11 +7,13 @@
 
 
 class ParserwbPipeline(object):
+    
     def process_item(self, item, spider):
         price_data = item['price_data']
-        price_data['current'] = price_data['current'].strip()
+        price_data['current'] = float(price_data['current'].strip().split('₽')[0].replace('\xa0',''))
         if price_data['original'] == None:
             price_data['original'] = price_data['current']
         else:
-            price_data['original'] = price_data['original'].strip()
+            price_data['original'] = float(price_data['original'].strip().split('₽')[0].replace('\xa0',''))
         return item
+
